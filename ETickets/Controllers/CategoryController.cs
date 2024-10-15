8f529ff3767c914dfdb65a68fb97063a455bc3d0
Jo0e <ykj627@gmail.com>
2024-10-15 20:54:32 +0300

    Add Purple UI + Filter

--- ETickets/Controllers/CategoryController.cs
+++ ETickets/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using ETickets.Data;
+using ETickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,16 +7,44 @@ namespace ETickets.Controllers
 {
     public class Category : Controller
     {
-        ApplicationDbContext context =new ApplicationDbContext();
+        ApplicationDbContext context = new ApplicationDbContext();
         public IActionResult Index()
         {
             var categories = context.Categories.ToList();
+            ViewBag.Categories = categories;
             return View(categories);
         }
         public IActionResult Details(int id)
         {
-            var movies = context.Movies.Include(e => e.Cinema).Include(m=>m.Category).Where(c => c.Category.Id == id).ToList();
+            var movies = context.Movies.Include(e => e.Cinema).Include(m => m.Category).Where(c => c.Category.Id == id).ToList();
             return View(movies);
         }
+
+        public IActionResult Filter()
+        {
+            var categories = context.Categories.ToList();
+            ViewBag.Categories = categories;
+            var cinemas = context.Cinemas.ToList();
+            ViewBag.Cinemas = cinemas;
+            var movies = new List<Movie>(); // Start with an empty list
+            return View(movies);
+
+        }
+
+        [HttpPost]
+        public IActionResult Filter(int CategoryId, int CinemaId, MovieStatus AvailabilityId)
+        {
+            var movie = context.Movies.Include(e => e.Cinema).Include(m => m.Category)
+                .Where(c => c.Category.Id == CategoryId && c.Cinema.Id==CinemaId && c.MovieStatus==AvailabilityId)
+                .ToList();
+
+            var categories = context.Categories.ToList();
+            ViewBag.Categories = categories;
+
+            var cinemas = context.Cinemas.ToList();
+            ViewBag.Cinemas = cinemas;
+            return View(movie);
+        }
+
     }
 }
